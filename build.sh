a540d1db197b0546ac2312fff9ccc74785a84138
Auston Stewart <auston@shedbuilt.net>
2018-01-10 16:39:45 -0500

    Updating to 3.6.4

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-CXX="/usr/bin/g++"              \
 ./configure --prefix=/usr       \
             --enable-shared     \
             --with-system-expat \
             --with-system-ffi   \
-            --with-ensurepip=yes
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-chmod -v 755 ${SHED_FAKEROOT}/usr/lib/libpython3.6m.so
-chmod -v 755 ${SHED_FAKEROOT}/usr/lib/libpython3.so
+            --with-ensurepip=yes || return 1
+make -j $SHED_NUMJOBS || return 1
+make DESTDIR="$SHED_FAKEROOT" install || return 1
+chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libpython3.6m.so"
+chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libpython3.so"
