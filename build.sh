51194aebf2b1f189165048692a29a12d5a524435
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:27:47 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
             --with-system-expat \
             --with-system-ffi   \
             --with-ensurepip=yes && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1
-chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libpython3.6m.so"
-chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libpython3.so"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.6m.so"
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.so"
