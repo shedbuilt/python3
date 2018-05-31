eeb7869bab6649da2ef063302dea9a573186f8d6
Auston Stewart <auston@shedbuilt.net>
2018-05-30 14:11:40 -1000

    Updating to v3.6.5

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
+# Configure
 ./configure --prefix=/usr       \
             --enable-shared     \
             --with-system-expat \
             --with-system-ffi   \
-            --with-ensurepip=yes && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.6m.so"
-chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.so"
+            --with-ensurepip=yes &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.6m.so" &&
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.so" || exit 1
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    wget https://docs.python.org/ftp/python/doc/3.6.5/python-3.6.5-docs-html.tar.bz2 &&
+    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}/html" &&
+    tar --strip-components=1  \
+        --no-same-owner       \
+        --no-same-permissions \
+        -C "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}/html" \
+        -xvf python-3.6.5-docs-html.tar.bz2
+else
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Install Python 3's package manager
 python3 -m ensurepip
