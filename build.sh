90639435603b7f6c2b91b1076654f0a20ce0e92d
Auston Stewart <auston@shedbuilt.net>
2018-08-14 12:48:52 -1000

    Updating to 3.7.0

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
-SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Configure
 ./configure --prefix=/usr       \
             --enable-shared     \
@@ -10,17 +9,17 @@ SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 # Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install &&
-chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.6m.so" &&
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.7m.so" &&
 chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libpython3.so" || exit 1
 # Install Documentation
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
-    wget https://docs.python.org/ftp/python/doc/3.6.5/python-3.6.5-docs-html.tar.bz2 &&
-    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}/html" &&
+    wget https://docs.python.org/ftp/python/doc/3.7.0/python-3.7.0-docs-html.tar.bz2 &&
+    install -v -dm755 "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}/html" &&
     tar --strip-components=1  \
         --no-same-owner       \
         --no-same-permissions \
-        -C "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}/html" \
-        -xvf python-3.6.5-docs-html.tar.bz2
+        -C "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}/html" \
+        -xvf python-3.7.0-docs-html.tar.bz2
 else
-    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+    rm -rf "${SHED_FAKE_ROOT}${SHED_PKG_DOCS_INSTALL_DIR}"
 fi
