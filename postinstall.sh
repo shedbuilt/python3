d9b649510bcc26f116ea05c1270dc365d566f7aa
Auston Stewart <auston@shedbuilt.net>
2018-08-15 08:08:52 -1000

    Making a symlink from python to python3 when python3 is the only python installed

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Symlink python to python3 if none exists
+if [ ! -e /usr/bin/python ]; then
+    ln -s python3 /usr/bin/python || exit 1
+fi
 # Install Python 3's package manager
 python3 -m ensurepip
