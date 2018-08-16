d21d32891c16651f0ce39439943f4925041ffa92
Auston Stewart <auston@shedbuilt.net>
2018-08-15 21:07:24 -1000

    On second thought, don't blindly symlink python -> python3

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Symlink python to python3 if none exists
-if [ ! -e /usr/bin/python ]; then
-    ln -s python3 /usr/bin/python || exit 1
-fi
 # Install Python 3's package manager
 python3 -m ensurepip
